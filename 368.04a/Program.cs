12fc61aa65d8d2957f7401429897c8eb193e4091
Your Name <you@example.com>
2018-12-17 03:26:45 -0500

    STEP 2

--- 368.04a/Program.cs
+++ 368.04a/Program.cs
@@ -8,7 +8,31 @@ namespace _368._04a
 {
     class Program
     {
-        public static Sale[] Sales = new Sale[] { };
+
+        static Sale sale1 = new Sale("Coffee", "HyperLLC", 3.50, 300, "123 nowhere ave", true);
+        static Sale sale2 = new Sale("Coffee", "WinnersLTD", 3.50, 300, "123 anywhere ave", true);
+        static Sale sale3 = new Sale("Coffee", "WinnersLTD", 3.50, 400, "123 anywhere ave", false);
+        static Sale sale4 = new Sale("Vodka", "HyperLLC", 30, 30, "123 nowhere ave", true);
+        static Sale sale5 = new Sale("Vodka", "WinnersLTD", 30, 30, "123 anywhere ave", true);
+        static Sale sale6 = new Sale("Vodka", "WinnersLTD", 30, 40, "123 anywhere ave", false);
+
+        public static Sale[] Sales = new Sale[] {sale1, sale2, sale3, sale4, sale5, sale6 };
+
+        public static Double TotalProfit(Sale[] s, Func<Sale, bool> fsb, Func<Sale, double> fsd, Action<Sale, double> asd,
+                Action<Sale> asa)
+        {
+            double tp = 0;
+            foreach (Sale sale in s) {
+                if (fsb(sale))
+                {
+                    tp += fsd(sale);
+                    asd(sale, fsd(sale));
+                }
+                else
+                    asa(sale);
+            }
+            return tp;
+        }
 
         static void Main(string[] args)
         {
@@ -21,7 +45,7 @@ namespace _368._04a
             var teaFalse = from csale in Sales
                            where csale.Item.Equals("Tea") && !csale.ExpeditedShipping
                            select csale;
-            
+
 
             var totalAdress = from csale in Sales
                               where (csale.PricePerItem * csale.Quantity) > 100.00
@@ -37,13 +61,6 @@ namespace _368._04a
                           orderby n.TotalPrice
                           select n;
 
-
-
-
-
-
-
-
             //Extension
             var ppiG102 = Sales.Where(s => s.PricePerItem > 10.0);
             var quant12 = Sales.Where(s => s.Quantity == 1).OrderByDescending(s => s.PricePerItem);
@@ -56,6 +73,21 @@ namespace _368._04a
                 TotalPrice = s.PricePerItem * s.Quantity,
                 Adress = s.ExpeditedShipping ? s.Address + " EXPEDITE" : s.Address
             }).OrderBy(s2 => s2.TotalPrice);
+
+
+            ////STEP 2////////////////
+            Console.WriteLine(TotalProfit(Sales, s => s.Item.Equals("Coffee"), s => s.PricePerItem * s.Quantity * .80,
+                (s, s2) => Console.WriteLine($"Coffee item for {s}, total profit: {s2}"), s => Console.WriteLine("Non-Coffee item, Skipping")));
+
+            Console.WriteLine(TotalProfit(Sales, s => s.Quantity > 1, s => s.ExpeditedShipping ? s.PricePerItem * s.Quantity + 20 :
+                  s.PricePerItem * s.Quantity,
+                (s, s2) =>
+                {
+                    if (s.ExpeditedShipping)
+                        Console.WriteLine($"Expedited Shipping sale of {s} - Extra $20 profit");
+                    else
+                        Console.WriteLine();
+                }, s => System.IO.File.WriteAllText(@"C:\Users\Public\TestFolder\WriteText.txt", $"Single Order Item: {s.Item}")));
 
         }
     }
--- 368.04a/Sale.cs
+++ 368.04a/Sale.cs
@@ -8,6 +8,23 @@ namespace _368._04a
 {
     public class Sale
     {
+        private string v1;
+        private string v2;
+        private double v3;
+        private int v4;
+        private string v5;
+        private bool v6;
+
+        public Sale(string v1, string v2, double v3, int v4, string v5, bool v6)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+        }
+
         public String Item { get; set; }
         public String Customer { get; set; }
         public double PricePerItem { get; set; }
@@ -15,5 +32,7 @@ namespace _368._04a
         public String Address { get; set; }
         public bool ExpeditedShipping { get; set; }
         
+
+
     }
 }
